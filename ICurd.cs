c7d8ef0299634ddf64451c831d702722d6967714
Muhammad Faraz Ansar <49414010+muhammadfaraz6640@users.noreply.github.com>
2020-04-06 21:50:19 +0500

    Backend Classes

---
+++ ICurd.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DayCareWebsite.Classes
+{
+    interface ICurd<T>
+    {
+        void Add(T s);
+        void Update(T s);
+        void Delete(T s);
+        T Search(T s);
+        List<T> GetIds();
+    }
+}
